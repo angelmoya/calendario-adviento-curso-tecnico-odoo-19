b3a10477917a477059df8c6d94516a9c7d0a61d5
angelmoya <amoyapardo@gmail.com>
2025-11-30 16:22:41 +0100

    Dia 2

---
+++ addons/library/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': "Library App",
+    'summary': """
+        Módulo de gestión de libros y préstamos para fines didácticos.""",
+    'description': """
+Módulo de ejemplo desarrollado como parte de un curso de desarrollo en Odoo.
+        Incluye la gestión básica de:
+        - Libros (Creación, Edición).
+        - Autores.
+        - Préstamos de libros a socios (clientes).
+        - Ejemplos de campos simples, relaciones y lógica de negocio (Python).
+    """,
+    'author': "Angel Moya",
+    'website': "https://angelmoya.es",
+    'category': 'Customization',
+    'version': '19.0.1.0.0',
+    'depends': ['sale_management', 'purchase', 'stock', 'account'],
+}
